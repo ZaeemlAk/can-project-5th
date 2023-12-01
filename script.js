3963c7d807bd6ae8cd13cb983f49682c9ef672de
ZaeemlAk <152154210+ZaeemlAk@users.noreply.github.com>
2023-12-01 15:36:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let section = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        } else {
+            sec.classList.remove('show-animate');
+        }
+    });
+});
